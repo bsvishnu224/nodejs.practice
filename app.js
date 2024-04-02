852274a54cccb78e93d98393821d60b8852264ae
bsvishnu224 <bsvvreddy@gmail.com>
2024-04-02 03:59:19 +0000

    error

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const addDays = require('date-fns/addDays')
+
+const app = express()
+
+app.get('/', (request, respose) => {
+  const result = new Date()
+
+  const date = addDays(
+    new Date(result.getFullYear(), result.getMonth(), result.getDate()),
+    100,
+  )
+
+  respose.sand(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
+})
+
+module.exports = app
